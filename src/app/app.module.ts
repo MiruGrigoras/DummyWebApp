17ae4aa2313276ca61efd7af5a5844dd7a767654
miruna.grigoras <miruna.grigoras@bearingpoint.com>
2020-05-19 22:45:55 +0300

    Setting up basic imports/changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+const AppRoutes: Routes = [
+  {path:'', redirectTo:'main', pathMatch:'full'},
+  {path:'main', component: MainComponent, children:[
+      {path:'', redirectTo: 'calculator'},
+      {path:'calculator', component: CalculatorComponent},
+      {path:'wiki', component: WikiComponent}
+  ]},
+  {path: '**', redirectTo:'main'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    CalculatorComponent,
+    WikiComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(AppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
